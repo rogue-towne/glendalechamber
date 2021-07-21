fc179489d34e2d5b1f746ce539d72f9f26956ba4
Rose Towne <69937144+rogue-towne@users.noreply.github.com>
2021-07-21 12:53:56 -0600

    Delete chambersite directory

--- chambersite/js/form.js
+++
@@ -1,8 +0,0 @@
-
-function selectResponse() {
-	const s = document.querySelector('#selected')
-	const sel = document.querySelector('#selectbrowser');
-	s.style.display = "block";
-	s.textContent = sel.value;
-	
-}
