9fb853dace92a221ee18701af81ba90ab0c16122
rogue-towne <69937144+rogue-towne@users.noreply.github.com>
2021-07-21 14:24:28 -0600

    fixed nav

--- js/toggleMenu.js
+++ js/toggleMenu.js
@@ -3,5 +3,5 @@ const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 
 toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('toggle-button')
+    navbarLinks.classList.toggle('toggle')
 })
