7556a0cdce52191a93031c0e374b05bae30a8e1e
Rose Towne <69937144+rogue-towne@users.noreply.github.com>
2021-07-21 12:49:10 -0600

    Add files via upload

---
+++ chambersite/js/form.js
@@ -0,0 +1,8 @@
+
+function selectResponse() {
+	const s = document.querySelector('#selected')
+	const sel = document.querySelector('#selectbrowser');
+	s.style.display = "block";
+	s.textContent = sel.value;
+	
+}
