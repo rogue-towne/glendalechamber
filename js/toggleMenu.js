1e821e95eec59ab69609fd92d472160fc4bffc45
rogue-towne <69937144+rogue-towne@users.noreply.github.com>
2021-07-21 23:42:15 -0600

    update

--- js/toggleMenu.js
+++ js/toggleMenu.js
@@ -1,13 +1,8 @@
 //Toggle Hamburger Menu
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navbarLinks = document.getElementsByClassName('navbar-links')[0];
-const weatherAlertLink = document.querySelector('weather-alert');
-const alertLink = document.querySelector('on');
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('toggle')
  
-})
-alertLink.addEventListener('click', () => {
-    weatherAlertLink.classList.toggle('off')
 })
