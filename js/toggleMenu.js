e17c026f0bcb5c4cee814716c824a5aa4bf52dcc
rogue-towne <69937144+rogue-towne@users.noreply.github.com>
2021-07-21 23:12:38 -0600

    update

--- js/toggleMenu.js
+++ js/toggleMenu.js
@@ -1,7 +1,13 @@
 //Toggle Hamburger Menu
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+const weatherAlertLink = document.querySelector('weather-alert');
+const alertLink = document.querySelector('on');
 
 toggleButton.addEventListener('click', () => {
     navbarLinks.classList.toggle('toggle')
+ 
+})
+alertLink.addEventListener('click', () => {
+    weatherAlertLink.classList.toggle('off')
 })
---
+++ js/weather.js
@@ -0,0 +1,11 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/onecall?lat=33.5387&lon=-112.186&units=imperial&exclude=minutely&appid=ffe8fd08fbf90ef22ca04d78481b8036"
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+      console.log(jsObject)
+      const weather = jsObject
+       document.getElementById('alert').textContent = weather.alerts[0].event;
+  });
+
+
+
